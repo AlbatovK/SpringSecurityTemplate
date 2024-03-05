8046ee4cfd5764d5d5e0db5d12a5998c982a3103
Konstantin Albatov <albatovkonstantin@yandex.ru>
2024-03-05 23:11:56 +0300

    :lock: Authorization in Swagger-UI

--- src/main/kotlin/com/albatros/springsecurity/config/api/OpenApiConfig.kt
+++ src/main/kotlin/com/albatros/springsecurity/config/api/OpenApiConfig.kt
@@ -1,10 +1,13 @@
 package com.albatros.springsecurity.config.api
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -30,5 +33,14 @@ class OpenApiConfig(private val apiInfo: ApiInfoConfig) {
                 description = apiInfo.externalDocsDescription
                 url = apiInfo.externalDocsUrl
             }
+        ).addSecurityItem(
+            SecurityRequirement().addList("Bearer Authentication")
+        ).components(
+            Components().addSecuritySchemes(
+                "Bearer Authentication",
+                SecurityScheme().type(SecurityScheme.Type.HTTP)
+                    .bearerFormat("JWT")
+                    .scheme("bearer")
+            )
         )
 }
